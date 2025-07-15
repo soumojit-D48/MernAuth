a549bb87871c895b3f5dea29a830e005640efca5
soumojit-D48 <soumojitdas107@gmail.com>
2025-07-16 01:44:32 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 4000
 connectDB()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://mernauth-frontend-bkwh.onrender.com']
 
 app.use(express.json())
 app.use(cookieParser())
